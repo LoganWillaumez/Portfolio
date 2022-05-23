ee3a8acea463c1aea48c7e04c3b46770818b54e5
LoganW <logan.willaumez@gmail.com>
2022-05-23 13:24:05 +0200

    [FEAT](sidebutton): finalise side button

--- Components/Layout.jsx
+++ Components/Layout.jsx
@@ -1,8 +1,9 @@
+import { IconButton } from '@mui/material';
 import { useState } from 'react';
 import { Footer } from './Footer';
 import { Header } from './Header';
 import { MenuSocial } from './MenuSocial';
-
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 export const Layout = ({ children }) => {
   const [open, setOpen] = useState(false);
   const openTrue = () => {
@@ -14,6 +15,7 @@ export const Layout = ({ children }) => {
   return (
     <>
       <MenuSocial open={open} openTrue={openTrue} openFalse={openFalse} />
+
       <Header />
       {children}
     </>
--- Components/MenuSocial.jsx
+++ Components/MenuSocial.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, IconButton } from '@mui/material';
 import { Paper } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
@@ -6,84 +6,107 @@ import EmailIcon from '@mui/icons-material/Email';
 import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 import ClickAwayListener from '@mui/material/ClickAwayListener';
 import ListIcon from '@mui/icons-material/List';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 export const MenuSocial = ({ open, openTrue, openFalse }) => {
+  console.log(`🚀 ~ open`, open);
   return (
     <>
       <ClickAwayListener onClickAway={openFalse}>
-        <Paper
-          component={Paper}
-          onClick={() => openTrue()}
-          sx={{
-            width: '80px',
-            height: '250px',
-            position: 'fixed',
-            left: { xs: '-40px', lg: '-5px' }, //-5 ok
-            top: { xs: '15%', lg: '40%' },
-            transform: 'translate(0, -50%)',
-            backgroundColor: '#FB8402',
-            zIndex: '10',
-            overflow: 'hidden',
-            transition: 'all 250ms ease-in-out',
-            transformOrigin: 'left top',
-            transform: { xs: 'scaleY(0.2)', lg: 'scaleY(1)' },
-            ...(open && {
-              transform: 'scale(1)',
-              transform: 'translateX(35px)',
-            }),
-          }}
-        >
-          <Box
-            height={'100%'}
+        <Box>
+          <IconButton
+            type='button'
+            onClick={() => openTrue()}
             sx={{
-              padding: '2rem 0 2rem 0',
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '1.5rem',
-              justifyContent: 'center',
-              alignItems: 'center',
-              opacity: { xs: '0', lg: '1' },
-              transform: { xs: 'scale(0)', lg: 'scale(1)' },
-              transition: 'all 250ms east-in-out',
+              position: 'fixed',
+              top: { xs: '15%', lg: '40%' },
+              zIndex: '50',
+              height: '50px',
+              padding: '0',
+              display: { xs: 'block', lg: 'none' },
+              transition: 'all 250ms ease',
               ...(open && {
-                opacity: '1',
-                transform: 'scale(1)',
+                transform: 'translateX(35px)',
+                opacity: '0',
               }),
             }}
           >
-            <a
-              style={{ color: 'white' }}
-              href='https://github.com/LoganWillaumez'
-              target='_blank'
-              rel='noopener noreferrer'
+            <ArrowRightIcon sx={{ fontSize: '40px' }} />
+          </IconButton>
+          <Paper
+            onClick={() => openTrue()}
+            component={Paper}
+            sx={{
+              width: '80px',
+              height: '250px',
+              position: 'fixed',
+              left: { xs: '-40px', lg: '-5px' }, //-5 ok
+              top: { xs: '15%', lg: '40%' },
+              transform: 'translate(0, -50%)',
+              backgroundColor: '#FB8402',
+              zIndex: '10',
+              overflow: 'hidden',
+              transition: 'all 250ms ease-in-out',
+              transformOrigin: 'left top',
+              transform: { xs: 'scaleY(0.2)', lg: 'scaleY(1)' },
+              ...(open && {
+                transform: 'scale(1)',
+                transform: 'translateX(35px)',
+              }),
+            }}
+          >
+            <Box
+              height={'100%'}
+              sx={{
+                padding: '2rem 0 2rem 0',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '1.5rem',
+                justifyContent: 'center',
+                alignItems: 'center',
+                opacity: { xs: '0', lg: '1' },
+                transform: { xs: 'scale(0)', lg: 'scale(1)' },
+                transition: 'all 250ms east-in-out',
+                ...(open && {
+                  opacity: '1',
+                  transform: 'scale(1)',
+                }),
+              }}
             >
-              <GitHubIcon />
-            </a>
-            <a
-              style={{ color: 'white' }}
-              href='https://www.linkedin.com/in/loganwillaumez/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <LinkedInIcon />
-            </a>
-            <a
-              style={{ color: 'white' }}
-              href='mailto:logan.willaumez@gmail.com'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <EmailIcon />
-            </a>
-            <a
-              style={{ color: 'white' }}
-              href='tel:0683485118'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <PhoneIphoneIcon />
-            </a>
-          </Box>
-        </Paper>
+              <a
+                style={{ color: 'white' }}
+                href='https://github.com/LoganWillaumez'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <GitHubIcon />
+              </a>
+              <a
+                style={{ color: 'white' }}
+                href='https://www.linkedin.com/in/loganwillaumez/'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <LinkedInIcon />
+              </a>
+              <a
+                style={{ color: 'white' }}
+                href='mailto:logan.willaumez@gmail.com'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <EmailIcon />
+              </a>
+              <a
+                style={{ color: 'white' }}
+                href='tel:0683485118'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <PhoneIphoneIcon />
+              </a>
+            </Box>
+          </Paper>
+        </Box>
       </ClickAwayListener>
     </>
   );
