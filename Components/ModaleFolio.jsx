59558d5a3c05daa4943ccb6be8220b45b1552722
LoganW <logan.willaumez@gmail.com>
2022-05-23 20:31:37 +0200

    [FEAT](increase-modalFolio): add icons

--- Components/ModaleFolio.jsx
+++ Components/ModaleFolio.jsx
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import { flexbox } from '.pnpm/@mui+system@5.7.0_t4r7icl7x3elshpaxc4xm7jrem/node_modules/@mui/system';
+import Image from 'next/image';
 
 const style = {
   position: 'absolute',
@@ -32,10 +33,12 @@ export default function ModaleFolio({
   img,
   desc,
   url,
+  icon,
 }) {
   //   const [open, setOpen] = React.useState(false);
   //   const handleOpen = () => setOpen(true);
   //   const handleClose = () => setOpen(false);
+  console.log(`üöÄ ~ icon`, icon);
 
   return (
     <div>
@@ -45,64 +48,100 @@ export default function ModaleFolio({
         aria-labelledby='modal-modal-title'
         aria-describedby='modal-modal-description'
       >
-        <Box sx={style}>
-          <Box
-            component='img'
-            sx={{
-              width: '100%',
-              // height: { xs: '30vh', lg: '40vh' },
-              height: '30vh',
-              objectFit: 'cover',
-            }}
-            src={img}
-          ></Box>
-          <Typography
-            sx={{
-              textAlign: 'center',
-              pt: 2,
-              fontWeight: '600',
-              fontSize: { xs: 'calc(1vw + 1vh + 1vmin)', lg: '2rem' },
-            }}
-            id='modal-modal-title'
-            component='h2'
-          >
-            {title}
-          </Typography>
-          <Typography
-            id='modal-modal-description'
-            sx={{
-              mt: 2,
-              pr: 3,
-              pl: 5,
-              pb: 2,
-              maxWidth: '600px',
-              fontSize: {
-                xs: 'calc(1vw + 1vh + 0.5vmin)',
-                md: 'calc(1vw + 1vh + 0.1vmin)',
-                lg: '1.5rem',
-              },
-              textAlign: 'justify',
-              fontWeight: '300',
-            }}
-          >
-            {desc}
-          </Typography>
-          <a
-            style={{
-              width: '100%',
-              textAlign: 'center',
-              margin: '0 auto',
-              paddingBottom: '1rem',
-              color: 'white',
-              bottom: '0',
-              position: 'absolute',
-            }}
-            href={url}
-            rel='noopener noreferrer'
-            target='_blank'
-          >
-            more info
-          </a>
+        <Box>
+          <Box sx={style}>
+            <Box
+              component='img'
+              sx={{
+                width: '100%',
+                // height: { xs: '30vh', lg: '40vh' },
+                height: '30vh',
+                objectFit: 'cover',
+              }}
+              src={img}
+            ></Box>
+            <Box sx={{ position: 'relative' }}>
+              <Typography
+                sx={{
+                  textAlign: 'center',
+                  pt: 2,
+                  fontWeight: '600',
+                  fontSize: { xs: 'calc(1vw + 1vh + 1vmin)', lg: '2rem' },
+                }}
+                id='modal-modal-title'
+                component='h2'
+              >
+                {title}
+              </Typography>
+
+              <Typography
+                id='modal-modal-description'
+                sx={{
+                  mt: 2,
+                  pr: 3,
+                  pl: 5,
+                  pb: 2,
+                  maxWidth: '600px',
+                  fontSize: {
+                    xs: 'calc(1vw + 1vh + 0.5vmin)',
+                    md: 'calc(1vw + 1vh + 0.1vmin)',
+                    lg: '1.5rem',
+                  },
+                  textAlign: 'justify',
+                  fontWeight: '300',
+                }}
+              >
+                {desc}
+              </Typography>
+            </Box>
+            <a
+              style={{
+                width: '100%',
+                textAlign: 'center',
+                margin: '0 auto',
+                paddingBottom: '1rem',
+                color: 'white',
+                bottom: '0',
+                position: 'absolute',
+              }}
+              href={url}
+              rel='noopener noreferrer'
+              target='_blank'
+            >
+              more info
+            </a>
+            <Box
+              sx={{
+                // height: '20px',
+                // width: '100px',
+                // backgroundColor: 'red',
+                position: 'absolute',
+                display: 'flex',
+                gap: '1rem',
+                bottom: '3rem',
+              }}
+            >
+              {icon?.map((pic, i) => {
+                return (
+                  <Box
+                    key={pic + i}
+                    sx={{
+                      height: { xs: '20px', lg: '40px' },
+                      width: { xs: '20px', lg: '40px' },
+                      position: 'relative',
+                    }}
+                  >
+                    <Image
+                      src={pic}
+                      alt={pic}
+                      layout='fill'
+                      objectFit='contain'
+                    />
+                  </Box>
+                );
+              })}
+            </Box>
+          </Box>
         </Box>
       </Modal>
     </div>
--- Components/Portfolio.jsx
+++ Components/Portfolio.jsx
@@ -65,6 +65,7 @@ export default function TitlebarImageList() {
         img={modalData?.img}
         desc={modalData?.desc}
         url={modalData?.url}
+        icon={modalData?.icon}
       />
     </ImageList>
   );
@@ -79,49 +80,7 @@ const itelgata = [
     rows: 2,
     cols: 2,
     featured: true,
-    icon: [],
-  },
-  {
-    img: 'https://user-images.githubusercontent.com/60406970/168473609-d7a7a30d-6694-486f-b655-e54099a9265d.png',
-    title: 'Netflix clone',
-    desc: 'A netflix clone, super fun to create and complexe to do. Mega fun ! ',
-    url: 'https://github.com/LoganWillaumez/Netflix-clone',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: 'https://user-images.githubusercontent.com/60406970/168473758-37114546-de05-4e88-beea-4c452176056b.png',
-    title: 'Circles',
-    author: '@helloimnik',
-  },
-  {
-    img: 'https://user-images.githubusercontent.com/60406970/168473161-680a96fc-2cd3-4323-b6a9-8f07cc194987.png',
-    title: 'Lusco',
-    desc: 'Project made for training about query and tailwindcss. Really fun to do ! ',
-    url: 'https://github.com/LoganWillaumez/Lusco',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://user-images.githubusercontent.com/60406970/168473609-d7a7a30d-6694-486f-b655-e54099a9265d.png',
-    title: 'Netflix clone',
-    desc: 'A netflix clone, super fun to create and complexe to do. Mega fun ! ',
-    url: 'https://github.com/LoganWillaumez/Netflix-clone',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: 'https://user-images.githubusercontent.com/60406970/168473758-37114546-de05-4e88-beea-4c452176056b.png',
-    title: 'Circles',
-    author: '@helloimnik',
-  },
-  {
-    img: 'https://user-images.githubusercontent.com/60406970/168473161-680a96fc-2cd3-4323-b6a9-8f07cc194987.png',
-    title: 'Lusco',
-    desc: 'Project made for training about query and tailwindcss. Really fun to do ! ',
-    url: 'https://github.com/LoganWillaumez/Lusco',
-    rows: 2,
-    cols: 2,
-    featured: true,
+    icon: ['/imgs/react-2.svg', '/imgs/tailwind-css-2.svg', '/imgs/vitejs.svg'],
   },
   {
     img: 'https://user-images.githubusercontent.com/60406970/168473609-d7a7a30d-6694-486f-b655-e54099a9265d.png',
