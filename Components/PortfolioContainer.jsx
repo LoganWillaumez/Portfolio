ddd67b5902d873abf4a3d8474359f445ef41e8de
Logan Willaumez <logan.willaumez@gmail.com>
2022-05-27 15:56:43 +0200

    Update README.md

--- Components/PortfolioContainer.jsx
+++ Components/PortfolioContainer.jsx
@@ -4,7 +4,7 @@ import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import { useState } from 'react';
 import ModaleFolio from './ModaleFolio';
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import Image from 'next/image';
 
 export default function TitlebarImageList({ projectData, info }) {
@@ -21,44 +21,54 @@ export default function TitlebarImageList({ projectData, info }) {
       >
         Portfolio
       </Typography>
-      <ImageList
+      <Box
         sx={{
-          width: '100%',
-          paddingTop: '2%',
+          overflow: 'auto',
+          height: '70vh',
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
         }}
-        cols={2}
       >
-        {projectData.map((item, i) => (
-          <ImageListItem
-            sx={{ aspectRatio: '16/9' }}
-            key={item.img}
-            onClick={(e) => {
-              toggleOpen();
-              setModalData(item);
-            }}
-          >
-            <Image
-              layout='fill'
-              objectFit='cover'
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={item.title}
-              loading='lazy'
-            />
-            <ImageListItemBar title={item.title} />
-          </ImageListItem>
-        ))}
-        <ModaleFolio
-          info={info}
-          toggleOpen={toggleOpen}
-          open={open}
-          title={modalData?.title}
-          img={modalData?.img}
-          desc={modalData?.desc}
-          url={modalData?.url}
-          icon={modalData?.icon}
-        />
-      </ImageList>
+        <ImageList
+          sx={{
+            width: '100%',
+            paddingTop: '2%',
+          }}
+          cols={2}
+        >
+          {projectData.map((item, i) => (
+            <ImageListItem
+              sx={{ aspectRatio: '16/9' }}
+              key={item.img}
+              onClick={(e) => {
+                toggleOpen();
+                setModalData(item);
+              }}
+            >
+              <Image
+                layout='fill'
+                objectFit='cover'
+                src={`${item.img}?w=248&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading='lazy'
+              />
+              <ImageListItemBar title={item.title} />
+            </ImageListItem>
+          ))}
+          <ModaleFolio
+            info={info}
+            toggleOpen={toggleOpen}
+            open={open}
+            title={modalData?.title}
+            img={modalData?.img}
+            desc={modalData?.desc}
+            url={modalData?.url}
+            icon={modalData?.icon}
+          />
+        </ImageList>
+      </Box>
     </>
   );
 }
